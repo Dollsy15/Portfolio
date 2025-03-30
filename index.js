0ecc2279d7f28b29fc4bfbea21d4e7abdf98fe60
Dollsy Rani <dollsyrani155@gmail.com>
2025-03-30 17:46:57 +0530

    js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var typed = new typed(".text", {
-    Strings:["Frontend Developer", "Youtuber", "Web Developer"],
+    Strings:["Frontend Developer", "Coder", "Web Developer"],
     typeSpeed: 100, 
     backSpeed: 100, 
     backDelay: 1000, 
